95b839aa2f4262f59476b93901c9706129ddabd0
augustoba <yo_augusto15@hotmail.com>
2023-12-21 18:24:10 -0300

    segundo commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.myapplication
 
 import android.content.ContentValues.TAG
-import android.nfc.Tag
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -21,25 +19,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.myapplication.ui.theme.MyApplicationTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,11 +46,14 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+val listaItems = listOf(Color.Black, Color.Red,Color.Yellow,Color.Black, Color.Green, Color.Cyan)
+val nombre: String = "Me Gusta"
 
 @Preview(showBackground = true)
 @Composable
 fun content(){
-
+    //sive para usar las variables en tiempo de ejecucion en tiempo real si no no se modifica en la vista
+    var likes by remember { mutableStateOf(0) }
 
 Column(modifier = Modifier
     .fillMaxSize()
@@ -68,7 +66,31 @@ Column(modifier = Modifier
     Space()
     Texto("front")
     Space()
+    //hace un for de la lista que le pasas y crea la row
+    LazyRow(modifier = Modifier
+        .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+        ){
+        items(listaItems) {item ->
+            Circulo(color = item)
+            Spacer(modifier = Modifier.width(10.dp))
+
+        }
+    }
+    Space()
     Row (modifier = Modifier
+        .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically){
+        Button(onClick = {
+            likes ++ }) {
+            Text(text = nombre)
+
+        }
+        Spacer(modifier = Modifier.width(10.dp))
+        Resultado(likes = likes)
+    }
+    /*Row (modifier = Modifier
         .fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceEvenly){
         Circulo(color =Color.Red)
@@ -76,11 +98,13 @@ Column(modifier = Modifier
         Circulo(color =Color.Yellow)
         Circulo(color =Color.Green)
 
-}
+}*/
 
 }
 
 }
+
+
 @Composable
 fun Texto(texto:String){
     Text(text = texto,
@@ -106,4 +130,9 @@ fun Circulo(color :Color){
 Box(modifier = Modifier
     .size(70.dp)
     .background(color, CircleShape))
+}
+
+@Composable
+fun Resultado(likes: Int){
+    Text(text= likes.toString(), fontWeight = FontWeight.Bold, fontSize = 50.sp)
 }
